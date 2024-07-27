b80b08ab9a865e40b37ad0fbe5187d6ce2901dc9
NIRESH SHRESTHA <dewebkiller@gmail.com>
2024-07-27 11:30:54 +0545

    added gap field and other admin details and doc tab

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -1,40 +1,47 @@
 <?php
-function apm_add_admin_menu() {
-    add_menu_page( 
-        'Payment Icons', 
-        'Payment Icons', 
-        'manage_options', 
-        'accepted-payment-methods', 
-        'apm_admin_page', 
-        'dashicons-money-alt' 
+function apm_add_admin_menu()
+{
+    add_menu_page(
+        'Payment Icons',
+        'Payment Icons',
+        'manage_options',
+        'accepted-payment-methods',
+        'apm_admin_page',
+        'dashicons-money-alt'
     );
 }
-add_action( 'admin_menu', 'apm_add_admin_menu' );
+add_action('admin_menu', 'apm_add_admin_menu');
 
-function apm_admin_page() {
+function apm_admin_page()
+{
     $default_methods = [];
     $methods = get_option('apm_payment_methods', $default_methods);
-    
+
     if (!is_array($methods)) {
         $methods = $default_methods;
     }
-    $default_settings = array('alignment' => 'left', 'size' => 'medium', 'tooltip' => 'yes');
+    $default_settings = array('alignment' => 'left', 'size' => 'medium', 'tooltip' => 'yes', 'icon_spacing' => 10);
     $settings = get_option('apm_settings', $default_settings);
-    
+
     $settings = wp_parse_args($settings, $default_settings);
     include(plugin_dir_path(__FILE__) . 'plugin-header.php');
-    ?>
+?>
     <div class="wrap apm-admin-page">
-        <h1>Accepted Payment Methods</h1>
         <h2 class="nav-tab-wrapper">
             <a href="#tab-1" class="nav-tab nav-tab-active">Accepted Payment Methods</a>
             <a href="#tab-2" class="nav-tab">Settings</a>
+            <a href="#tab-3" class="nav-tab">Documentation</a>
         </h2>
         <div id="tab-1" class="tab-content">
-            <h3>Accepted Payment Methods</h3>
+            <div class="dwk-notice dwk-notice-inner">
+                <div class="dwk-notice__content">
+                    <h3>Add payment methods</h3>
+                    <p>Add the payment method icons/images and drag and drop the blocks for ordering the payment methods.</p>
+                </div>
+            </div>
             <ul id="payment-methods-list">
-                <?php foreach ($methods as $method): ?>
-                    <?php if (is_array($method) && isset($method['name'], $method['icon'])): ?>
+                <?php foreach ($methods as $method) : ?>
+                    <?php if (is_array($method) && isset($method['name'], $method['icon'])) : ?>
                         <li class="payment-method-item" data-method="<?php echo esc_attr($method['name']); ?>">
                             <img src="<?php echo esc_url($method['icon']); ?>" alt="<?php echo esc_attr(ucfirst($method['name'])); ?>">
                             <span><?php echo ucfirst(str_replace('-', ' ', $method['name'])); ?></span>
@@ -54,7 +61,12 @@ function apm_admin_page() {
             <p id="apm-save-message" class="hidden"></p>
         </div>
         <div id="tab-2" class="tab-content" style="display:none;">
-            <h3>Settings</h3>
+            <div class="dwk-notice dwk-notice-inner">
+                <div class="dwk-notice__content">
+                    <h3>Settings</h3>
+                    <p>Experiment with the various settings available for the plugins to achieve optimal results.</p>
+                </div>
+            </div>
             <form method="post" action="options.php">
                 <?php settings_fields('apm_settings_group'); ?>
                 <table class="form-table">
@@ -67,6 +79,7 @@ function apm_admin_page() {
                                 <option value="right" <?php selected($settings['alignment'], 'right'); ?>>Right</option>
                             </select>
                         </td>
+                        <td>Aligmnent of icons</td>
                     </tr>
                     <tr valign="top">
                         <th scope="row">Size</th>
@@ -77,6 +90,15 @@ function apm_admin_page() {
                                 <option value="large" <?php selected($settings['size'], 'large'); ?>>Large</option>
                             </select>
                         </td>
+                        <td>
+                            <p class="description">
+                                <?php
+                                echo wp_kses_post(
+                                    __('Predefined size of icons. <em>Note: You can overwrite via theme style.</em>', 'your-text-domain')
+                                );
+                                ?>
+                            </p>
+                        </td>
                     </tr>
                     <tr valign="top">
                         <th scope="row">Display Tooltip</th>
@@ -86,12 +108,41 @@ function apm_admin_page() {
                                 <option value="no" <?php selected($settings['tooltip'], 'no'); ?>>No</option>
                             </select>
                         </td>
+                        <td>Display the tooltip or not</td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row"><label for="apm_icon_spacing"><?php esc_html_e('Spacing', 'your-text-domain'); ?></label></th>
+                        <td>
+                            <input type="number" class="small-text" name="apm_settings[icon_spacing]" id="apm_icon_spacing" value="<?php echo esc_attr($settings['icon_spacing']); ?>">
+                        </td>
+                        <td>
+                            <p class="description"><?php esc_html_e('Gap between icons in px', 'your-text-domain'); ?></p>
+                        </td>
                     </tr>
                 </table>
                 <?php submit_button(); ?>
             </form>
         </div>
+        <div id="tab-3" class="tab-content">
+            <div class="dwk-notice dwk-notice-inner">
+                <div class="dwk-notice__content">
+                    <h3>Documentation</h3>
+                    <p>The basic guide on how to implement this plugin in your theme.</p>
+                </div>
+            </div>
+            <ol>
+                <li>
+                    <p><strong>Plugin shortcode: (Use this on a post or page)</strong></p>
+                    <p>Use the shortcode [dwk_apm_methods] to display them on your site</p>
+                </li>
+                <li>
+                    <p><strong>Template Tag: (Use this in a template php file)</strong></p>
+                    <pre>&lt;?php echo do_shortcode('[dwk_apm_methods]');?&gt;</pre>
+                </li>
+            </ol>
+
+        </div>
     </div>
-    <?php
+<?php
     include(plugin_dir_path(__FILE__) . 'plugin-footer.php');
 }
--- includes/plugin-header.php
+++ includes/plugin-header.php
@@ -2,9 +2,7 @@
   <div class="row">
     <div class="logo-wrap col-md-6">
       <div class="title">
-        <p>
-          <a href="https://www.dewebkiller.com/" target="_blank"><img src="https://www.dewebkiller.com/dwk/images/logo.png" alt="dewebkiller" class="dwk-logo" width="300"></a>
-        </p>
+        <a href="https://www.dewebkiller.com/" target="_blank"><img src="https://www.dewebkiller.com/dwk/images/logo.png" alt="dewebkiller" class="dwk-logo" width="200"></a>
       </div>
     </div>
 
@@ -12,5 +10,15 @@
       <a href="https://buymeacoffee.com/dewebkiller/" target="_blank" class="dwk-btn dashicons-before dashicons-heart"> Support</a>
       <a href="#" class="dwk-btn btn2 dashicons-before dashicons-star-filled"> Rate us</a>
     </div>
+  </div>
+</div>
+<div class="clear"></div>
+<div class="dwk-notice">
+  <div class="dwk-notice__aside">
+  <span class="dashicons dashicons-info-outline"></span>
+  </div>
+  <div class="dwk-notice__content">
+  <h1>Showcase Payment Methods</h1>
+    <p>Display the payment method icons and images in you WordPress website like never before.</p>
   </div>
 </div>
